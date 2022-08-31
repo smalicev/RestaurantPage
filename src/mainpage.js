14da7cbd85fe04368e65d8a65a19b8c47b818aab
Serge M <srdjanmalicevic7@outlook.com>
2022-08-30 22:39:31 -0400

    webpack setup

---
+++ src/mainpage.js
@@ -0,0 +1,11 @@
+
+
+
+page = document.querySelector('#content');
+
+const header = document.createElement('div');
+
+const maintent = document.createElement('div');
+
+maintent.classList.add('main');
+header.classList.add('header');
