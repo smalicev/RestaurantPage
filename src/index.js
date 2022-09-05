6fbffc31c3557ce790673fa3a38edeacde11fc1f
Serge M <srdjanmalicevic7@outlook.com>
2022-09-05 11:48:47 -0400

    add webpack setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import _ from 'lodash';
+import './style.css';
+
+import { pageLoad } from './mainpage.js';
 console.log("test24");
+pageLoad();
+
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,11 +1,33 @@
 
 
 
-page = document.querySelector('#content');
+const pageLoad = function pageLoad(){
+const page = document.querySelector('.content');
 
 const header = document.createElement('div');
-
 const maintent = document.createElement('div');
-
+const footer = document.createElement('div');
 maintent.classList.add('main');
 header.classList.add('header');
+footer.classList.add('footer');
+
+
+const menubut = document.createElement('button');
+const aboutbut = document.createElement('button');
+const contactbut = document.createElement('button');
+
+menubut.classList.add('Restbut');
+aboutbut.classList.add('Restbut');
+contactbut.classList.add('Restbut');
+
+page.appendChild('header');
+page.appendChild('maintent');
+page.appendChild ('footer');
+
+header.appendChild('menubut');
+header.appendChild('aboutbut');
+header.appendChild('contactbut');
+
+}
+
+export { pageLoad };
